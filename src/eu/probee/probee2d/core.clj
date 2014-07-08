57ad6ee7b597d782d4fd7c246c8b2f999a737af3
Claus Engel-Christensen <cec@casalogic.dk>
2014-07-08 23:48:44 +0200

    Added new imports and a multimethod for wrapping of Java Color class

--- src/eu/probee/probee2d/core.clj
+++ src/eu/probee/probee2d/core.clj
@@ -1,5 +1,29 @@
 (ns eu.probee.probee2d.core
-  (:import (javax.swing JFrame)))
+  (:import (javax.swing JFrame)
+           (java.awt Color Graphics Toolkit)
+           (java.awt.image BufferedImage FilteredImageSource RGBImageFilter)
+           (java.io File IOException)
+           (javax.imageio ImageIO)))
+
+(defmulti color
+  (fn [c] (type c)))
+
+(defmethod color clojure.lang.APersistentMap
+  [{:keys [r g b]}]
+  (Color. r g b))
+
+(defmethod color clojure.lang.APersistentVector
+  [[r g b]]
+  (Color. r g b))
+
+(defmethod color clojure.lang.Keyword
+  [c]
+  (eval (symbol (str "Color/" (name c)))))
+
+(defmethod color java.lang.String
+  [c]
+  (eval (symbol (str "Color/" c))))
+
 
 (defprotocol window-actions
   (show [this])
