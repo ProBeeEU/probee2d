66d6ddfd349da744bf5df459ba35fc5d47ce3889
Claus Engel-Christensen <cec@casalogic.dk>
2014-07-14 00:15:47 +0200

    Rewrote the color helper function, to fix an error when refered from another ns

--- src/eu/probee/probee2d/util.clj
+++ src/eu/probee/probee2d/util.clj
@@ -1,6 +1,20 @@
 (ns eu.probee.probee2d.util
   (:import (java.awt Color)))
 
+(def colors {:black (Color/black)
+             :blue (Color/blue)
+             :cyan (Color/cyan)
+             :dark-gray (Color/darkGray)
+             :gray (Color/gray)
+             :green (Color/green)
+             :light-gray (Color/lightGray)
+             :magenta (Color/magenta)
+             :orage (Color/orange)
+             :pink (Color/pink)
+             :red (Color/red)
+             :white (Color/white)
+             :yellow(Color/yellow)})
+
 (defmulti color
   (fn [c] (type c)))
 
@@ -14,8 +28,8 @@
 
 (defmethod color clojure.lang.Keyword
   [c]
-  (eval (symbol (str "Color/" (name c)))))
+  (c colors))
 
 (defmethod color java.lang.String
   [c]
-  (eval (symbol (str "Color/" c))))
+  (color (keyword c)))
