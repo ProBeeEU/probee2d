ebff4245b30955f725421b7ce62ecc93b947c070
Claus Engel-Christensen <cec@casalogic.dk>
2014-08-11 14:57:11 +0200

    Changed keyboard into a record
      - Also added function for single and multi key presses

--- src/eu/probee/probee2d/core.clj
+++ src/eu/probee/probee2d/core.clj
@@ -231,15 +231,50 @@
   [game-loop]
   (future-cancel game-loop))
 
-(defn- key-name [event]
-  (keyword (str/lower-case (KeyEvent/getKeyText (.getKeyCode event)))))
+(defn- key-name [key-code]
+  (keyword (str/lower-case (KeyEvent/getKeyText key-code))))
+
+(defmulti key-pressed
+  (fn [key-states key] (type key)))
+
+(defmethod key-pressed clojure.lang.Keyword
+  [key-states key]
+  (true? (key key-states)))
+
+(defmethod key-pressed java.lang.Character
+  [key-states key]
+  (true? ((keyword (str key)) key-states)))
+
+(defmethod key-pressed java.lang.String
+  [key-states key]
+  (true? ((keyword key) key-states)))
+
+(defmethod key-pressed java.lang.Number
+  [key-states key]
+  (true? ((key-name key) key-states)))
+
+(defmethod key-pressed clojure.lang.APersistentVector
+  [key-states keys]
+  (every? #(key-pressed key-states %) keys))
+
+(defprotocol PKeyboard
+  (pressed? [this key]))
+
+(defrecord Keyboard
+    [key-states]
+  PKeyboard
+  (pressed? [this key] (key-pressed @key-states key)))
 
 (defn add-keyboard
   [{:keys [window]}]
   (let [pressed-keys (atom {})]
-    (.addKeyListener window (proxy [KeyAdapter] []
-                              (keyPressed [event]
-                                (swap! pressed-keys assoc (key-name event) true))
-                              (keyReleased [event]
-                                (swap! pressed-keys assoc (key-name event) false))))
-    pressed-keys))
+    (doto window
+      (.setFocusTraversalKeysEnabled false)
+      (.addKeyListener (proxy [KeyAdapter] []
+                         (keyPressed [event]
+                           (swap! pressed-keys assoc
+                                  (key-name (.getKeyCode event)) true))
+                         (keyReleased [event]
+                           (swap! pressed-keys assoc
+                                  (key-name (.getKeyCode event)) true)))))
+    (->Keyboard pressed-keys)))
