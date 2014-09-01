a43fbf29cde00f95a109f0f4ca749aaed618053a
Claus Engel-Christensen <cec@casalogic.dk>
2014-09-01 12:33:37 +0200

    Added new structure and functions for handling Sprites

--- src/eu/probee/probee2d/core.clj
+++ src/eu/probee/probee2d/core.clj
@@ -198,3 +198,15 @@
   `(let [stats# (when ~use-stats (game-statistics (or (:statistics-interval ~options) 1)))
          w# (assoc (window ~title ~width ~height ~input-devices) :stats stats#)]
     (assoc w# :game-loop (game-loop w# ~update-fn ~render-fn ~options stats#))))
+
+(defstruct Sprite :x :y :img :vel-x :vel-y)
+
+(defn sprite [x y img & [vel-x vel-y]]
+  (struct Sprite x y img (or vel-x 0) (or vel-y 0)))
+
+(defn move-sprite [{:keys [x y vel-x vel-y] :as sprite}]
+  (assoc sprite :x (+ x vel-x) :y (+ y vel-y)))
+
+(defn draw-sprite [{:keys [x y img] :as sprite} renderer]
+  (let [{:keys [width height]} img]
+    (draw img renderer (- x (/ width 2)) (- y (/ height 2)))))
